df38184fc078de26e31cba1e694e9e7c0cf6d38e
jaydenvo <jaydenvo@localhost.localdomain>
2019-05-07 09:47:11 +0800

    Implemented requirements for python client. Add functionality to basic server to receive and send messages

--- basic_server.c
+++ basic_server.c
@@ -5,7 +5,7 @@
 
 /**
  * @brief A simple example of a network server written in C implementing a basic echo
- * 
+ *
  * This is a good starting point for observing C-based network code but is by no means complete.
  * We encourage you to use this as a starting point for your project if you're not sure where to start.
  * Food for thought:
@@ -23,6 +23,56 @@
 
 #define BUFFER_SIZE 1024
 
+typedef struct
+{
+    int client_fd;
+    int num_lives;
+    char *client_id;
+} Client;
+
+void send_message(char *message, int destination_fd)
+{
+    int err = send(destination_fd, message, strlen(message), 0);
+    if (err < 0){
+        fprintf(stderr,"Client write failed\n");
+        exit(EXIT_FAILURE);
+    }
+}
+
+char* receive_message(int sender_fd)
+{
+    char *buf = calloc(BUFFER_SIZE, sizeof(char)); // Clear our buffer so we don't accidentally send/print garbage
+    int read = recv(sender_fd, buf, BUFFER_SIZE, 0);    // Try to read from the incoming client
+    if (read < 0){
+        fprintf(stderr,"Client read failed\n");
+        exit(EXIT_FAILURE);
+    }
+    return buf;
+}
+
+void parse_message(char *message, Client client )
+{
+    if(strstr(message,"INIT"))
+    {
+        char *welcome_message = calloc(strlen("WELCOME,") + strlen(client.client_id), sizeof(char))
+        strcpy(welcome_message,"WELCOME,");
+        strcat(welcome_message,client.client_id));
+        send_message(welcome_message, atoi(client.client_id));
+    }
+
+    else if(strstr(message,"MOV"))
+    {
+        //Update life of that client
+    }
+
+    else
+    {
+        fprintf(stderr,"Received an unexpected response from player")
+    }
+}
+
+Client *connected_clients;
+
 int main (int argc, char *argv[]) {
     if (argc < 2) {
         fprintf(stderr,"Usage: %s [port]\n",argv[0]);
@@ -31,7 +81,7 @@ int main (int argc, char *argv[]) {
 
     int port = atoi(argv[1]);
 
-    int server_fd, client_fd, err, opt_val;
+    int server_fd, client_fd, err, opt_val, client_id;
     struct sockaddr_in server, client;
     char *buf;
 
@@ -53,13 +103,13 @@ int main (int argc, char *argv[]) {
     if (err < 0){
         fprintf(stderr,"Could not bind socket\n");
         exit(EXIT_FAILURE);
-    } 
+    }
 
     err = listen(server_fd, 128);
     if (err < 0){
         fprintf(stderr,"Could not listen on socket\n");
         exit(EXIT_FAILURE);
-    } 
+    }
 
     printf("Server is listening on %d\n", port);
 
@@ -67,100 +117,26 @@ int main (int argc, char *argv[]) {
         socklen_t client_len = sizeof(client);
         // Will block until a connection is made
         client_fd = accept(server_fd, (struct sockaddr *) &client, &client_len);
-
+        client_id = rand() % 900 + 100; //Generate a 3 digit id number
+        Client client = {client_fd,client_id};
         if (client_fd < 0) {
             fprintf(stderr,"Could not establish new connection\n");
             exit(EXIT_FAILURE);
-
-        } 
-        
-        /**
-        The following while loop contains some basic code that sends messages back and forth
-        between a client (e.g. the socket_client.py client). 
-        
-        The majority of the server 'action' will happen in this part of the code (unless you decide
-        to change this dramatically, which is allowed). The following function names/definitions will 
-        hopefully provide inspiration for how you can start to build up the functionality of the server. 
-        
-        parse_message(char *){...} : 
-            * This would be a good 'general' function that reads a message from a client and then 
-            determines what the required response is; is the client connecting, making a move, etc. 
-            * It may be useful having an enum that is used to track what type of client message is received
-            (e.g. CONNECT/MOVE etc.) 
-            
-        send_message() {...}: 
-            * This would send responses based on what the client has sent through, or if the server needs 
-            to send all clients messages
-        
-        play_game_round() {...}: Implements the functionality for a round of the game
-            * 'Roll' the dice (using a random number generator) and then check if the move made by the user
-            is correct
-            * update game state depending on success or failure. 
-            
-        setup_game/teardown_game() {} : 
-            * this will set up the initial state of the game (number of rounds, players
-            etc.)/ print out final game results and cancel socket connections. 
-            
-        Accepting multiple connections (we recommend not starting this until after implementing some
-        of the basic message parsing/game playing): 
-            * Whilst in a while loop
-                - Accept a new connection 
-                - Create a child process 
-                - In the child process (which is associated with client), perform game_playing functionality
-                (or read the messages) 
-        **/
-        
-        while (true) {  
-            buf = calloc(BUFFER_SIZE, sizeof(char)); // Clear our buffer so we don't accidentally send/print garbage
-            int read = recv(client_fd, buf, BUFFER_SIZE, 0);    // Try to read from the incoming client
-
-            if (read < 0){
-                fprintf(stderr,"Client read failed\n");
-                exit(EXIT_FAILURE);
-            }
-
-            printf("%s\n", buf);
-
-            buf[0] = '\0';
-            sprintf(buf, "My politely respondance");
-
-            err = send(client_fd, buf, strlen(buf), 0); // Try to send something back
-            // printf("Client's message is: %s",buf);
-             sleep(5); //Wait 5 seconds
-
-            buf[0] = '\0';
-            sprintf(buf, "Let the games begin\n");
-
-            err = send(client_fd, buf, strlen(buf), 0); // Send another thing
-            if (err < 0){
-                fprintf(stderr,"Client write failed\n");
-                exit(EXIT_FAILURE);
-            }
-
-            read = recv(client_fd, buf, BUFFER_SIZE, 0); // See if we have a response
-
-            if (read < 0){
-                fprintf(stderr,"Client read failed\n");
-                exit(EXIT_FAILURE);
-            }
-
-            if (strstr(buf, "move") == NULL) {  // Check if the message contained 'move'
-                fprintf(stderr, "Unexpected message, terminating\n");
-                exit(EXIT_FAILURE);
-            }
-
-            buf[0] = '\0';
-            sprintf(buf, "You lose\n");
-
-            err = send(client_fd, buf, strlen(buf), 0); // Send our final response
-
-            if (err < 0){
-                    fprintf(stderr,"Client write failed\n");
-                    exit(EXIT_FAILURE);
-            }
-
-            free(buf);
         }
 
+        //Store a connected reference to the client
+        if(connected_clients == NULL)
+        {
+            continue;
+        }
+        else
+        {
+            continue;
+        }
+
+        while (true) {
+            char *response = receive_message(client_fd);
+            parse_message(response, client);
+        }
     }
 }
--- socket_client.py
+++ socket_client.py
@@ -1,22 +1,27 @@
-"""
-This is a simple example of a client program written in Python.
-Again, this is a very basic example to complement the 'basic_server.c' example.
-
-
-When testing, start by initiating a connection with the server by sending the "init" message outlined in 
-the specification document. Then, wait for the server to send you a message saying the game has begun. 
-
-Once this message has been read, plan out a couple of turns on paper and hard-code these messages to
-and from the server (i.e. play a few rounds of the 'dice game' where you know what the right and wrong 
-dice rolls are). You will be able to edit this trivially later on; it is often easier to debug the code
-if you know exactly what your expected values are. 
-
-From this, you should be able to bootstrap message-parsing to and from the server whilst making it easy to debug.
-Then, start to add functions in the server code that actually 'run' the game in the background. 
-"""
-
 import socket
 from time import sleep
+
+class UnexpectedResponseException(Exception):
+    """Raised when client receives an unexpected response"""
+    pass
+
+def make_move(client_id):
+    move = int(input('Select a move:\n 1 - Even\n 2 - Odd\n 3 - Doubles\n 4 - Contains "n" \n Move: '))
+    while move != 1 and move != 2 and move != 3 and move != 4:
+        move = input('Invalid input. Please re-enter')
+    if move == 1:
+        return client_id+",EVEN"
+    elif move == 2:
+        return client_id+",ODD"
+    elif move == 3:
+        return client_id+",DOUB"
+    else:
+        dice_number = int(input("Select a number from 1-6"))
+        while dice_number < 1 or dice_number > 6:
+            print("Value is not between 1-6")
+            dice_number = int(input("Select a number from 1-6"))
+        return client_id+",CON,"+dice_number
+
 # Create a TCP/IP socket
 sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
@@ -26,35 +31,52 @@ print ('connecting to %s port %s' % server_address)
 sock.connect(server_address)
 
 count=0
-message = 'This is the message.  It will be repeated.'.encode()
 try:
+    sock.sendall('INIT'.encode())
+    response = sock.recv(16)
+    #Client attempts to join midgame, client will attempt to reconnect every 10s
+    if response == "REJECT":
+        print("Unable to join game. A match is currently in progress")
+        print("Attempting to find a match .....")
+        while not response.startswith("WELCOME"):
+            sock.sendall('INIT'.encode())
+            response = sock.recv(16).decode()
+            sleep(10)
     while True:
-    
-        exit = False
-        # Send data
-        message = 'This is the message.  It will be repeated.'.encode()
-        print ('sending "%s"' % message)
-        sock.sendall(message)
-
-        # Look for the response
-        amount_received = 0
-        amount_expected = len(message)
-        
-        while amount_received < amount_expected:
-            data = sock.recv(1024)
-            amount_received += len(data)
-            mess = data.decode()
-            if "games" in mess:
-                print("The games have begun")
-                sock.sendall('231,MOV,CON,1'.encode()) # Client has ID 231
-            elif "You lose" in mess:
-                print("We lost, closing connection")
-                exit = True
-                break
-            else:
-                print ( 'received "%s"' % mess)
-        if exit:
-            break
-finally:    
-    print ('closing socket')
+      client_id = response.decode()[-3:]
+      print(client_id)
+      game_status = sock.recv(16)
+      #Server Notified not enough players for match
+      if game_status == "CANCEL":
+          print("Not enough players for match to begin. Exiting.....")
+          break
+      elif game_status.startswith("START"):
+          game_status = game_status.split(",")
+          num_lives = game_status[2]
+          print("Match with "+ game_status[1] + "players starting")
+          print("Starting Number of Lives: " + num_lives)
+          result = None;
+          while result != "ELIM" and result != "VIC":
+              move = make_move()
+              sock.sendall(move.encode())
+              result = sock.rev(16).decode()
+              if result == "PASS":
+                print("You have guessed correctly")
+              elif result == "FAIL":
+                  num_lives -= 1
+                  print("You have guessed correctly")
+                  print("Remaining Lives: " + num_lives)
+              else:
+                  raise UnexpectedResponseException("Unexpected Response "+ result)
+          if result == "ELIM":
+             print("You have been eliminated")
+          else:
+             print("You are the winner")
+      #Obtained an unexpected response
+      else:
+          raise UnexpectedResponseException("Unexpected Response " + game_status)
+except UnexpectedResponseException as e:
+    print(str(e))
+finally:
+    print('closing socket')
     sock.close()
